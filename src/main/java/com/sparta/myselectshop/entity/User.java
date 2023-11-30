f95b8375ea41ef489aa6fe56d7417bd00f69fd0a
yeongdo99 <atpsfka1@gmail.com>
2023-11-30 19:35:58 +0900

    카카오 사용자 정보로 회원가입 구현

--- src/main/java/com/sparta/myselectshop/entity/User.java
+++ src/main/java/com/sparta/myselectshop/entity/User.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @Setter
@@ -24,15 +27,29 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
-
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
     private UserRoleEnum role;
+
+    private Long kakaoId;
 
     public User(String username, String password, String email, UserRoleEnum role) {
         this.username = username;
         this.password = password;
         this.email = email;
         this.role = role;
+    }
+
+    public User(String username, String password, String email, UserRoleEnum role, Long kakaoId) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.kakaoId =kakaoId;
+    }
+
+    public User kakaoIdUpdate(Long kakaoId) {
+        this.kakaoId = kakaoId;
+        return this;
     }
 }
